69ee3250738ef54fe387a7819abf3d4064bb89bd
nocturnics <nocturnics@gmail.com>
2016-04-10 21:35:09 -0700

    Added files via upload

---
+++ nz/css/nztcss.php
@@ -0,0 +1,14 @@
+<?php
+defined('ACCESS_ALLOWED') or die('Restricted Access');
+echo '
+<!--Scripts-->
+<link rel="stylesheet" type="text/css" href="../css/nocs.css" />
+<link rel="stylesheet" type="text/css" href="../fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="../css/dropit.css"  />
+<script type="text/javascript" src="../js/jquery.js"></script>
+<script type="text/javascript" src="../fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+<script type="text/javascript" src="../js/dropit.js"></script>
+<!--<script type="text/javascript" src=""></script>-->
+
+	';
+?>
